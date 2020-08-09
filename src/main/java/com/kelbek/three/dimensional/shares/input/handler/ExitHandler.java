c9c7f7fe61daf63a9f0c5722b7167a8b9bcfdc16
kelbek <dev_kelbek@mail.ru>
2020-08-09 15:38:08 +0300

    small reafctoring input exit handler

--- src/main/java/com/kelbek/three/dimensional/shares/input/handler/ExitHandler.java
+++ src/main/java/com/kelbek/three/dimensional/shares/input/handler/ExitHandler.java
@@ -9,7 +9,8 @@ import static org.lwjgl.glfw.GLFW.*;
 public class ExitHandler implements GLFWKeyCallbackI {
     @Override
     public void invoke(long window, int key, int scancode, int action, int mods) {
-        if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+        if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE ) {
             glfwSetWindowShouldClose(window, true);
+        }
     }
 }
